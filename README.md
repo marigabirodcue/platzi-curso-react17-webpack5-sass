06214189db7e0e32e432ff752a1b2d2ab35e96de
marigabirodcue <rodriguezcmariaga@gmail.com>
2023-10-11 20:38:08 -0400

    Proyecto del curso de reat básico de Platzi con React 17, webpack, scss, babel: la app es Ract Shop que viene del proyecto Frontend Developer

---
+++ README.md
@@ -0,0 +1,9 @@
+# Platzi Project
+
+- Teacher: Oscar Barajas
+- Project with:
+- React 17,
+- Webpack,
+- scss,
+- Babel
+
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry:'./src/index.js',
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename:'bundle.js',
+        publicPath: '/',
+    },
+    mode: 'development',
+    resolve:{
+        extensions:['.js','.jsx'],
+        alias:{
+            '@components': path.resolve(__dirname, 'src/components/'),
+            '@containers': path.resolve(__dirname, 'src/containers/'),
+            '@icons': path.resolve(__dirname, 'src/assets/icons/'),
+            '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+            '@pages': path.resolve(__dirname, 'src/pages/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@hooks': path.resolve(__dirname, 'src/hooks/'),
+            '@context': path.resolve(__dirname, 'src/context/'),
+        }
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$|jsx/,
+                exclude:/node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            },
+            {
+                test:/\.html$/,
+                use:[
+                    {
+                        loader:'html-loader',
+                    }
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                        "style-loader",
+                        "css-loader",
+                        "sass-loader",
+                ],
+            },
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                type: 'asset'
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            //inject:true,
+            template:'./public/index.html',
+            filename:'./index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    devServer: {
+      //  allowedHosts: path.join(__dirname, 'dist'),
+       // compress: true,
+        historyApiFallback: true,
+       // port: 3005,
+    }
+    /* devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },*/
+}
